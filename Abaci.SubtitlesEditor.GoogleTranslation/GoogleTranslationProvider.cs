843e26a22b49a4f1dd027a6450e5a706fb7f7e28
JD Pierce <brkfstmnchr@gmail.com>
2023-03-15 19:26:44 +0700

    change targeting to netstd20; add project Abaci.SubtitlesEditor.GoogleTranslation
    change targeting to netstd20; add project Abaci.SubtitlesEditor.GoogleTranslation

---
+++ Abaci.SubtitlesEditor.GoogleTranslation/GoogleTranslationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Abaci.SubtitlesEditor.GoogleTranslation
+{
+    public class GoogleTranslationProvider : ITranslationProvider
+    {
+        public Task<string> TranslateAsync(string text, string target_language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Abaci.SubtitlesEditor/TextParser.cs
+++ Abaci.SubtitlesEditor/TextParser.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
+[assembly:InternalsVisibleTo("Abaci.SubtitlesEditor.Tests")]
 namespace Abaci.SubtitlesEditor
 {
     internal class TextParser
